96c25366b8c8f52258266ac11883c639358fa75b
rudy onfroy <rudy@onfroy.net>
2017-01-23 20:23:52 +0100

    add some reducers tests

--- tests/reducers.test.js
+++ tests/reducers.test.js
@@ -1,44 +1,80 @@
 import React from 'react';
 import reducers from 'src/reducers';
-import iniState from 'src/state';
+import initState from 'src/state';
+
+const cloneState  = (state) => {
+    return JSON.parse(JSON.stringify(state))
+};
 
 describe('Reducers - Initialization', () => {
-    const state = undefined;
+    const input = undefined;
     const action = {'type': undefined};
-    const newState = reducers(state, action);
+    const output = reducers(input, action);
 
     it('must have regex as string', () => {
-        expect(typeof newState.regex).toEqual('string');
+        expect(typeof output.regex).toEqual('string');
     });
 
     it('must have next_identifier as number', () => {
-        expect(typeof newState.next_identifier).toEqual('number');
+        expect(typeof output.next_identifier).toEqual('number');
     });
 
     it('must have navigation as array', () => {
-        expect(newState.navigation instanceof Array).toBeTruthy();
+        expect(output.navigation instanceof Array).toBeTruthy();
     });
 
     it('must have builder as object', () => {
-        expect(newState.builder instanceof Object).toBeTruthy();
+        expect(output.builder instanceof Object).toBeTruthy();
     });
 
     it('must have tester as object', () => {
-        expect(newState.tester instanceof Object).toBeTruthy();
+        expect(output.tester instanceof Object).toBeTruthy();
     });
 });
 
 describe('Reducers - Builder - Reset', () => {
+    const input = cloneState(initState);
+    input.builder.rules.push({});
     const action = {'type': 'REGEX_BUILDER_RESET'};
-    const newState = reducers(iniState, action);
+    const output = reducers(input, action);
 
     it('have no rules', () => {
-        expect(newState.builder.rules.length).toEqual(0);
+        expect(output.builder.rules.length).toEqual(0);
     });
 
     it('have no active option', () => {
-        for (var i = 0, len = newState.builder.options; i < len; i++) {
-            expect(newState.builder.options[i].active).toBeFalsy();
+        for (var i = 0, len = output.builder.options; i < len; i++) {
+            expect(output.builder.options[i].active).toBeFalsy();
+        }
+    });
+});
+
+describe('Reducers - Builder - Add rule', () => {
+    const input = cloneState(initState);
+    const nbInputRule = input.builder.rules.length;
+    const action = {'type': 'REGEX_BUILDER_ADD_RULE'};
+    const output = reducers(input, action);
+
+    it('have one more rule', () => {
+        expect(output.builder.rules.length).toEqual(nbInputRule + 1);
+    });
+});
+
+describe('Reducers - Builder - Remove rule', () => {
+    const identifier = 123;
+    const input = cloneState(initState);
+    input.builder.rules.push({identifier: identifier});
+    const nbInputRule = input.builder.rules.length;
+    const action = {'type': 'REGEX_BUILDER_REMOVE_RULE', 'rule_identifier': identifier};
+    const output = reducers(input, action);
+
+    it('have one less rule', () => {
+        expect(output.builder.rules.length).toEqual(nbInputRule - 1);
+    });
+
+    it("doesn't contain removed rule", () => {
+        for (var i = 0, len = output.builder.rules.length; i < len; i++) {
+            expect(output.builder.rules[i].identifier).not.toEqual(identifier);
         }
     });
 });
